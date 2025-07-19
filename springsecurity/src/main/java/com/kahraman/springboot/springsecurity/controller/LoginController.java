6d9d097f64771a44c74d728f0f603c52e899a231
hasan.kahraman <kahramanhasanb@gmail.com>
2025-07-19 15:08:27 +0200

    Login Controller and DemoSecurityConfig added basically

---
+++ springsecurity/src/main/java/com/kahraman/springboot/springsecurity/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.kahraman.springboot.springsecurity.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+
+        return "plain-login";
+    }
+
+}
--- springsecurity/src/main/java/com/kahraman/springboot/springsecurity/security/DemoSecurityConfig.java
+++ springsecurity/src/main/java/com/kahraman/springboot/springsecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,11 @@ package com.kahraman.springboot.springsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -23,4 +25,21 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(hasan, ali, julian);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                    configurer
+                            .anyRequest().authenticated()
+            )
+                .formLogin(form ->
+                        form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll());
+
+        return http.build();
+    }
+
 }
