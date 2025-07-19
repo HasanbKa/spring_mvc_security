803b2bdf18901d0433ca58f354467410ef0c221b
hasan.kahraman <kahramanhasanb@gmail.com>
2025-07-19 16:01:17 +0200

    fancy-login added for error message with bootstrap

--- springsecurity/src/main/java/com/kahraman/springboot/springsecurity/controller/LoginController.java
+++ springsecurity/src/main/java/com/kahraman/springboot/springsecurity/controller/LoginController.java
@@ -9,8 +9,7 @@ public class LoginController {
 
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage() {
-
-        return "plain-login";
+        return "fancy-login";
     }
 
 }
